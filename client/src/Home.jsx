7ab2a911e0fe18d8fc90d32557a7c877db6cb63c
bsester <124089310+bsester@users.noreply.github.com>
2023-12-08 18:26:51 -0600

    first column of homepage

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,14 +1,41 @@
 import Navbar from "./Navbar";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function Home()
 {
+    const [topCustomers, setTopCustomers] = useState([]);
+    useEffect(() => {
+        axios.get('http://localhost:8081/topCustomers')
+            .then(res => setTopCustomers(res.data))
+            .catch(err => console.log(err))
+    }, []);
+
     return (
 
         <div className="row">
             <div className="col-sm-4">
                 <h2 className = "text-center"> Top Customers </h2>
                 <div id = "customerResults">
-
+                    <table className = 'table table-striped'>
+                    <thead>
+                    <tr>
+                        <th> Name </th>
+                        <th> Sales </th>
+                    </tr>
+                    </thead>
+                        <tbody>
+                        {topCustomers.map((customer, index) =>
+                        {
+                            return <tr key ={index}>
+                                <td>{customer.CustomerName}</td>
+                                <td>${customer.Total}</td>
+                            </tr>
+                        })}
+                        </tbody>
+                    </table>
                 </div>
             </div>
             <div className="col-sm-4">
--- server/server.js
+++ server/server.js
@@ -14,6 +14,25 @@ const db = mysql.createConnection({
     }
 )
 
+
+app.get('/topCustomers', (req, res) =>
+{
+    const sql = "SELECT s.SalesID, c.CustomerName, i.ItemName, s.Quantity, i.ItemPrice * s.Quantity AS Total " +
+    "FROM sales s JOIN customer c ON s.CustomerID = c.CustomerID " +
+    "JOIN item i ON s.ItemID = i.ItemID " +
+    "ORDER BY `Total` DESC " +
+    "LIMIT 5";
+    db.query(sql, (err, result) =>
+    {
+        if (err)
+            console.log(err);
+        else
+            return res.json(result);
+    })
+})
+
+
+
 app.listen(8081, () =>
 {
     console.log("listening...");
